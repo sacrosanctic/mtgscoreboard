379a096c4c6ea06baea820e1f57d8bc6c3b1be0d
sacrosanctic <2974920+sacrosanctic@users.noreply.github.com>
2019-08-20 15:45:26 -0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,42 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer app>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>MTG</span>
+        <span class="font-weight-light">Scoreboard</span>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+          <v-btn class="primary--text" text to="/">Scoreboard</v-btn>
+          <v-btn class="primary--text" text to="/about">Control</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-content>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    //
+  })
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,62 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="6"
+          v-for="(player, i) in players"
+          :key="i"
+      >
+        <v-card
+        >
+          <v-card-title>{{player.name}}</v-card-title>
+          <v-card-text>
+            <v-list>
+              <v-list-item v-for="(item, j) in player.counter" :key="item.name">
+                <v-list-item-content>{{item.name}}: {{item.value}}</v-list-item-content>
+                <v-list-item-action>
+                  <v-card-actions>
+                    <v-btn @click="plusOne(i,j)">+1</v-btn>
+                  </v-card-actions>
+                </v-list-item-action>
+              </v-list-item>
+            </v-list>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      p1cmdr: 0,
+      players: [
+        {
+          name: "player1",
+          counter: [
+            { name: "life", value: 0 },
+            { name: "poison", value: 0 },
+            { name: "experience", value: 0 },
+            { name: "commander", value: 0 }
+          ]
+        },
+        {
+          name: "player2",
+          counter: [
+            { name: "life", value: 1 },
+            { name: "poison", value: 1 },
+            { name: "experience", value: 1 },
+            { name: "commander", value: 1 }
+          ]
+        }
+      ]
+    };
+  },
+  methods: {
+    plusOne(player, i) {
+      this.players[player].counter[i].value++;
+    }
+  }
+};
+</script>
